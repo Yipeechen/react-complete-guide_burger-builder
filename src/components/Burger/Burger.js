a691c4d732d8ea1e094d3e5338b89c5c5b4db2db
Yipeechen <yipeechen0418@gmail.com>
2019-12-19 10:05:24 +0800

    Calculating the ingredient sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  // * transformedIngredients is an array which is either empty or contains the jsx elements
   // keys(): extracts the keys of a given object and turns that into an array, so it gives you an array of the keys.
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       // Array(): is default javascript object, not react object. It can create an array with it. For example Array(3): will give you an array with three empty spaces basically, with three undefined space
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
       })
       // do not care the element itself, so use the underscore(_) as an argument name to indicate that it's a blank .
-    });
+    })
+    .reduce((array, element) => {
+      return array.concat(element);
+    }, []);
+    // [] is an initial value of this reduced value
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   
   return(
     <div className={classes.Burger}>

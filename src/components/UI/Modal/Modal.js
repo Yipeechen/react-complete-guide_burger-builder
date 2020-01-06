29b33425f550aedf3aa8fb6bec32bdbd756e549c
Yipeechen <yipeechen0418@gmail.com>
2020-01-06 09:33:53 +0800

    Improving performance: to make sure that modal doesn't update unnecessarily

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop
-      show={props.show}
-      clicked={props.modalClosed}
-    />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+  // the wrapping element(Modal) controls the updating of the wrapped element(OrderSummary).
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  componentDidUpdate () {
+    console.log('[Modal] DidUpdate');
+  }
+
+  render () {
+    return(
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modalClosed}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;

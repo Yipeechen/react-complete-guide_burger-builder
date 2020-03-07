e6fcb50a58ea94507a064511648ef5a81f227e94
Yipeechen <yipeechen0418@gmail.com>
2020-03-07 09:51:18 +0800

    Redirect to improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -16,21 +16,26 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          checkoutCanceled={this.checkoutCanceledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-          ingredients={this.props.ings}
-        />
-        <Route
-          path={this.props.match.url + '/contact-data'}
-          component={ContactData}
-          // pass history which we do get in the props of this render method here
-          // render={props => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)}
-        />
-      </div>
-    )
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            checkoutCanceled={this.checkoutCanceledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+            ingredients={this.props.ings}
+          />
+          <Route
+            path={this.props.match.url + '/contact-data'}
+            component={ContactData}
+            // pass history which we do get in the props of this render method here
+            // render={props => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)}
+          />
+        </div>
+      );
+    }
+
+    return summary;
   }
 }
 

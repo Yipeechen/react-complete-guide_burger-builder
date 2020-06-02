7082bb269cd789338bac5b8a4c57e9df8132aff9
Yipeechen <yipeechen0418@gmail.com>
2020-06-02 09:52:21 +0800

    Converting withErrorHandler HOC

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
@@ -6,56 +6,45 @@ import Aux from '../Aux/Aux';
 const withErrorHandler = (WrappedComponent, axios) => {
   // use class-base d component not functional component, bcz add the componentDidMount lifecycle hook to setup the axios interceptors
   //withErrorHandler creates these classes, is a class factory essentially
-  return class extends Component {
-    state = {
-      error: null
-    }
+  return props => {
+    const [error, setError] = useState(null);
 
-    // execute this code when this component here gets created
-    constructor (props) {
-      super(props);
-      this.reqInterceptor = axios.interceptors.request.use(req => {
-        this.setState({
-          error: null
-        });
-        // have to return the request config so that the request can continue and for the response
-        return req;
-      })
-      this.resInterceptor = axios.interceptors.response.use(
-        res => res,
-        error => {
-          this.setState({
-            error: error
-          });
-        }
-      )
-    }
+    const reqInterceptor = axios.interceptors.request.use(req => {
+      setError(null);
+      // have to return the request config so that the request can continue and for the response
+      return req;
+    });
+    const resInterceptor = axios.interceptors.response.use(
+      res => res,
+      err => {
+        setError(err);
+      }
+    );
 
     // executed at hte point of time, a component isn't required anymore, so when we reuse withErrorHandler in our application, we don't create more and more interceptors with old ones living on
-    componentWillUnmount() {
-      // remove the interceptors to prevent memory leaks.
-      axios.interceptors.request.eject(this.reqInterceptor);
-      axios.interceptors.response.eject(this.resInterceptor);
+    useEffect(() => {
+      // clean up function: use return function, means componentWillUnMount()
+      return () => {
+        // remove the interceptors to prevent memory leaks.
+        axios.interceptors.request.eject(reqInterceptor);
+        axios.interceptors.response.eject(resInterceptor);
+      };
+    }, [reqInterceptor, resInterceptor]);
+
+    const errorConfirmedHandler = () => {
+      setError(null);
     }
 
-    errorConfirmedHandler = () => {
-      this.setState({
-        error: null
-      });
-    }
-
-    render () {
       return (
         <Aux>
           <Modal
-            show={this.state.error}
-            modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null}
+            show={error}
+            modalClosed={errorConfirmedHandler}>
+            {error ? error.message : null}
           </Modal>
-          <WrappedComponent {...this.props} />
+          <WrappedComponent {...props} />
         </Aux>
       );
-    }
   }
 }
 
